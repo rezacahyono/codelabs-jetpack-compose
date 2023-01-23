6aa7fe123ac8612751e80f93f9944f533c0f012f
rezacahyono <rezacahyono1@gmail.com>
2023-01-23 19:53:53 +0700

    [Fix] fixing themes, and add expanded card animation in project Woof

--- Woof/app/src/main/java/com/rchyn/woof/MainActivity.kt
+++ Woof/app/src/main/java/com/rchyn/woof/MainActivity.kt
@@ -3,14 +3,22 @@ package com.rchyn.woof
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,7 +35,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WoofTheme {
+            WoofTheme(dynamicColor = false) {
                 WoofApp()
             }
         }
@@ -60,43 +68,99 @@ fun WoofList(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WoofCard(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.surfaceColorAtElevation(
+            4.dp
+        ) else MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp)
+    )
+
     ElevatedCard(
         modifier = modifier
             .fillMaxWidth()
             .padding(8.dp),
-        shape = RoundedCornerShape(50)
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.outlinedCardColors(containerColor = color)
     ) {
-        Row(modifier = modifier.padding(8.dp)) {
-            Image(
-                modifier = Modifier
-                    .size(64.dp)
-                    .clip(RoundedCornerShape(50)),
-                painter = painterResource(id = dog.imageResourceId),
-                contentDescription = stringResource(id = dog.name),
-                contentScale = ContentScale.Crop
-            )
-            Column(
+
+        Column(
+            modifier = modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
+            Row(
                 modifier = modifier
-                    .padding(start = 16.dp, end = 16.dp)
-                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = stringResource(id = dog.name),
-                    style = MaterialTheme.typography.titleMedium
+                Image(
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(RoundedCornerShape(50)),
+                    painter = painterResource(id = dog.imageResourceId),
+                    contentDescription = stringResource(id = dog.name),
+                    contentScale = ContentScale.Crop
                 )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = stringResource(id = R.string.years_old, dog.age),
-                    style = MaterialTheme.typography.bodySmall
-                )
+                Column(
+                    modifier = modifier
+                        .padding(start = 16.dp, end = 16.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = dog.name),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.years_old, dog.age),
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+                Spacer(modifier = modifier.weight(1f))
+                FilledTonalIconButton(
+                    onClick = { expanded = !expanded }
+                ) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                        contentDescription = stringResource(
+                            id = R.string.expand_button_content_description
+                        )
+                    )
+                }
+            }
+            if (expanded) {
+                DogHobby(dogHobby = dog.hobbies)
             }
         }
+    }
+}
+
+@Composable
+fun DogHobby(
+    @StringRes dogHobby: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.labelMedium
+        )
+        Text(
+            text = stringResource(id = dogHobby),
+            style = MaterialTheme.typography.bodySmall
+        )
     }
 }
 
--- Woof/app/src/main/java/com/rchyn/woof/ui/theme/Theme.kt
+++ Woof/app/src/main/java/com/rchyn/woof/ui/theme/Theme.kt
@@ -1,10 +1,15 @@
 package com.rchyn.woof.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 
 private val LightColors = lightColorScheme(
@@ -21,18 +26,18 @@ private val LightColors = lightColorScheme(
     tertiaryContainer = md_theme_light_tertiaryContainer,
     onTertiaryContainer = md_theme_light_onTertiaryContainer,
     error = md_theme_light_error,
-    errorContainer = md_theme_light_errorContainer,
     onError = md_theme_light_onError,
+    errorContainer = md_theme_light_errorContainer,
     onErrorContainer = md_theme_light_onErrorContainer,
+    outline = md_theme_light_outline,
     background = md_theme_light_background,
     onBackground = md_theme_light_onBackground,
     surface = md_theme_light_surface,
     onSurface = md_theme_light_onSurface,
     surfaceVariant = md_theme_light_surfaceVariant,
     onSurfaceVariant = md_theme_light_onSurfaceVariant,
-    outline = md_theme_light_outline,
-    inverseOnSurface = md_theme_light_inverseOnSurface,
     inverseSurface = md_theme_light_inverseSurface,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
     inversePrimary = md_theme_light_inversePrimary,
     surfaceTint = md_theme_light_surfaceTint,
 )
@@ -52,18 +57,18 @@ private val DarkColors = darkColorScheme(
     tertiaryContainer = md_theme_dark_tertiaryContainer,
     onTertiaryContainer = md_theme_dark_onTertiaryContainer,
     error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
     onError = md_theme_dark_onError,
+    errorContainer = md_theme_dark_errorContainer,
     onErrorContainer = md_theme_dark_onErrorContainer,
+    outline = md_theme_dark_outline,
     background = md_theme_dark_background,
     onBackground = md_theme_dark_onBackground,
     surface = md_theme_dark_surface,
     onSurface = md_theme_dark_onSurface,
     surfaceVariant = md_theme_dark_surfaceVariant,
     onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
     inverseSurface = md_theme_dark_inverseSurface,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
     inversePrimary = md_theme_dark_inversePrimary,
     surfaceTint = md_theme_dark_surfaceTint,
 )
@@ -71,16 +76,28 @@ private val DarkColors = darkColorScheme(
 @Composable
 fun WoofTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable() () -> Unit
+  dynamicColor: Boolean = true,
+  content: @Composable () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = useDarkTheme
+        }
+    }
 
-  MaterialTheme(
-    colorScheme = colors,
-    content = content
-  )
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
